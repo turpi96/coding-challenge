01faa016d652293f45a6550a00dfb548367da237
Jean-Philippe <turpi0496@gmail.com>
2019-06-04 21:21:46 -0400

    test

--- index.js
+++ index.js
@@ -42,14 +42,12 @@ express()
       {
         const client = await pool.connect()
         const result = await client.query('SELECT ninja_equivalent FROM buzzword_ninja_name_equiv_table WHERE buzzword = $1', [req.body.buzzword]);
-        const results = { 'name': (result) ? result.rows : null};
+        const results = { 'name': (result) ? result.rows : null };
 
         // parsed_results.name + " " + parsed_results.name
         //var ninja_name_string = results.name + " " + results.name;
         //res.end(ninja_name_string);
         //var ninja_name = JSON.parse(ninja_name_string);
-        var tmp = results.name;
-        console.log('tmp');
 
         res.end(JSON.stringify(results));
         client.release();
