42c4c7dbc22fd0ad7cb18b0678f15d414db3d1ac
Jean-Philippe <turpi0496@gmail.com>
2019-06-04 20:46:33 -0400

    Clean de trucs non-utilisés et ajout du mot clé REQUIRED au text box

--- index.js
+++ index.js
@@ -18,71 +18,42 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  //.get('/', (req, res) => res.render('pages/index'))
   .get('/', (req, res) => res.redirect('/ninjify'))
-  /*.get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect()
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })*/
+
   .get('/ninjify', (req, res) => {
       res.render('pages/ninjify', {qs: req.query});
   })
+
   .post('/ninjify', urlencodedParser, async (req, res) => {
     // Prepare output in JSON format
     /*response = {
       complete_ninja_name: 
     };*/
 
+    //Vérifie si le champs du formulaire est vide
     if(!req.body.buzzword)
     {
       res.render('pages/index');
     }
     else
     {
-      try {
+
+      try 
+      {
         const client = await pool.connect()
         const result = await client.query('SELECT ninja_equivalent FROM buzzword_ninja_name_equiv_table WHERE buzzword = $1', [req.body.buzzword]);
         const results = { 'Name': (result) ? result.rows : null};
-        //res.render('pages/db', results );
+
         res.end(JSON.stringify(results));
         client.release();
-      } catch (err) {
+      } 
+      catch (err) 
+      {
         console.error(err);
         res.send("Error " + err);
       }
 
-
-
-
-
-      /*
-      response = {
-        name:req.body.buzzword
-      };
-      */
-      //res.end(JSON.stringify(response));
-
     }
-  }) //Probablement ce que je vais devoir utiliser pour la partie post du formulaire?
+  }) 
 
-  //.get('/cool', (req, res) => res.send(cool()))
-  //.get('/times', (req, res) => res.send(showTimes()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-
-  /*showTimes = () => {
-    let result = ''
-    const times = process.env.TIMES || 5
-    for (i = 0; i < times; i++) {
-      result += i + ' '
-    }
-    return result;
-  }*/
